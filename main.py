e4f4794e91acd25f6f90f5aabc461de1d2983a2f
rauner <rauner@pik-potsdam.de>
2022-03-17 15:52:59 +0100

    add CNN

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from handlers.lemon import LemonMarketsAPI
 
 
 
+
 #load_dotenv()
 
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 import yfinance as yf
-import datetime
+from datetime import datetime, timedelta, date
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -25,7 +29,7 @@ from model import *
 #start = os.environ.get("startDate")
 #
 
-start = pd.to_datetime('2018-08-01')
+
 isin = ['ETH-USD']
 testShare = 0.05
 
@@ -34,33 +38,223 @@ modelMetric = 'MSE'
 
 epochs = 20
 batch_size = 16
+interval = 2 #time steps in minutes
 
-
-data = yf.download(isin, start=start, end=datetime.date.today())
+# intraday data only available from yf for the last 60 days
+data = yf.download(isin, start=date.today() -  timedelta(days=59), end=date.today(), interval= str(interval) + 'm')
 print(data.head())
 
 # normalize the dataset
 #scaler = MinMaxScaler(feature_range=(0, 1))
 #data = scaler.fit_transform(data)
 
+
+# include days and weeks as cos features
+data = data.reset_index(level=0)
+seconds = data['Datetime'].map(pd.Timestamp.timestamp)
+
+
+day = 24*60*60/interval
+week = 7*day
+
+data['day cos'] = np.cos(seconds * (2 * np.pi / day))
+data['week cos'] = np.cos(seconds * (2 * np.pi / week))
+data = data.drop('Datetime', axis = 1)
+
 # split into train and test
-Y = data['Adj Close']
-X = data.drop('Adj Close', axis=1)
+n = len(data)
+train_df = data[0:int(n*0.7)]
+val_df = data[int(n*0.7):int(n*0.9)]
+test_df = data[int(n*0.9):]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=testShare)
-
-# train model
-model = LongShortTermMemory(X_train)
-
-callback = get_callback(model)
-
-#callback
-callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='min', verbose=1)
-
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=modelMetric)
-model.fit(X_train, Y_train, epochs, batch_size, validation_data=(X_test, Y_test),
-                        callbacks=callback)
-
-#use XGBOOST model
+num_features = data.shape[1]
 
 
+# normalization, simple mean and variance
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df=train_df, val_df=val_df, test_df=test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+
+  def split_window(self, features):
+      inputs = features[:, self.input_slice, :]
+      labels = features[:, self.labels_slice, :]
+      if self.label_columns is not None:
+        labels = tf.stack(
+            [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+            axis=-1)
+
+      # Slicing doesn't preserve static shape information, so set the shapes
+      # manually. This way the `tf.data.Datasets` are easier to inspect.
+      inputs.set_shape([None, self.input_width, None])
+      labels.set_shape([None, self.label_width, None])
+
+      return inputs, labels
+  def plot(self, model=None, plot_col='Adj Close', max_subplots=3):
+      inputs, labels = self.example
+      plt.figure(figsize=(12, 8))
+      plot_col_index = self.column_indices[plot_col]
+      max_n = min(max_subplots, len(inputs))
+      for n in range(max_n):
+        plt.subplot(max_n, 1, n+1)
+        plt.ylabel(f'{plot_col} [normed]')
+        plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                 label='Inputs', marker='.', zorder=-10)
+
+        if self.label_columns:
+          label_col_index = self.label_columns_indices.get(plot_col, None)
+        else:
+          label_col_index = plot_col_index
+
+        if label_col_index is None:
+          continue
+
+        plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                    edgecolors='k', label='Labels', c='#2ca02c', s=64)
+        if model is not None:
+          predictions = model(inputs)
+          plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                      marker='X', edgecolors='k', label='Predictions',
+                      c='#ff7f0e', s=64)
+
+        if n == 0:
+          plt.legend()
+
+      plt.xlabel('Time [2m]')
+      plt.show()
+
+  def make_dataset(self, data, batch_size = 32):
+      data = np.array(data, dtype=np.float32)
+      ds = tf.keras.utils.timeseries_dataset_from_array(
+          data=data,
+          targets=None,
+          sequence_length=self.total_window_size,
+          sequence_stride=1,
+          shuffle=True,
+          batch_size=batch_size,)
+
+      ds = ds.map(self.split_window)
+
+      return ds
+
+  @property
+  def train(self):
+      return self.make_dataset(self.train_df)
+
+  @property
+  def val(self):
+      return self.make_dataset(self.val_df)
+
+  @property
+  def test(self):
+      return self.make_dataset(self.test_df)
+
+  @property
+  def example(self):
+      """Get and cache an example batch of `inputs, labels` for plotting."""
+      result = getattr(self, '_example', None)
+      if result is None:
+        # No example batch was found, so get one from the `.train` dataset
+        result = next(iter(self.train))
+        # And cache it for next time
+        self._example = result
+      return result
+
+
+MAX_EPOCHS = 20
+
+def compile_and_fit(model, window, patience=2):
+  early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                    patience=patience,
+                                                    mode='min')
+
+  model.compile(loss=tf.losses.MeanSquaredError(),
+                optimizer=tf.optimizers.Adam(),
+                metrics=[tf.metrics.MeanAbsoluteError()])
+
+  history = model.fit(window.train, epochs=MAX_EPOCHS,
+                      validation_data=window.val,
+                      callbacks=[early_stopping])
+  return history
+
+# implement CNN
+CONV_WIDTH = 10
+LABEL_WIDTH = 24
+INPUT_WIDTH = LABEL_WIDTH + (CONV_WIDTH - 1)
+wide_conv_window = WindowGenerator(
+    input_width=INPUT_WIDTH,
+    label_width=LABEL_WIDTH,
+    shift=1,
+    label_columns=['Adj Close'])
+
+
+
+conv_model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(filters=32,
+                           kernel_size=(CONV_WIDTH,),
+                           activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1),
+])
+
+
+
+history = compile_and_fit(conv_model, wide_conv_window)
+
+
+
+
+
+wide_conv_window.plot(conv_model)
+
+'''
+clean, make class and so on nice
+implement that its forcasts the next h with ARNN
+
+
+# implement XGBOOST
+# implement random tree
+# implement bagging
+# boosting
+
+
+'''
